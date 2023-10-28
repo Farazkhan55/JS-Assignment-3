77bb65a7aa33ad3613e9d9809fca0da08afec4a4
Farazkhan55 <fk417145@gmail.com>
2023-10-28 22:53:27 +0500

    newassignment

---
+++ app.js
@@ -0,0 +1,88 @@
+// var nam = ("hello")
+// var num1 = prompt("enter your name")
+// console.log(nam + " " + num1)
+
+
+// var num1 = +prompt("enter number")
+// var num2 = +prompt("enter nmuber")
+// console.log(num1 + num2)
+   
+
+
+
+
+
+
+
+var dish = prompt("enter menu")
+if(dish=="chicken" ){
+    console.log("avaiilable")
+}
+else if (dish=="samose" ) {
+    console.log("avaiilable")
+}
+else if (dish=="fries") {
+    console.log("avaiilable")
+}
+else if (dish==="zinger") {
+    console.log("avaiilable")
+}
+else if (dish=="brost") {
+    console.log("avaiilable")
+}
+else {
+    console.log("not eligible")
+}
+
+
+
+
+
+
+// var prog = prompt("Enter Name")
+
+// if(prog=="JavaScript"){
+//     alert("JS")
+// }
+// else if(prog=="python"){
+//     alert("py")
+// }
+// else if(prog=="C++"){
+//     alert("CC")
+// }
+// else{
+//     alert("Search On Google")
+// }
+
+
+
+// var ice = prompt("enter cream name")
+// if (ice=="falooda") {
+//     alert("this is here")
+    
+// }
+
+// else if (ice=="icecream") {
+//     alert("this is here")
+    
+// }
+    
+// else if (ice=="khoyaa") {
+//     alert("this is here")
+    
+// }
+    
+// else if (ice=="rabri") {
+//     alert("this is here")
+    
+// }
+    
+// else if (ice=="makhan") {
+//     alert("this is here")
+    
+// }
+    
+// else {
+//     alert("this flavour is not eligible")
+    
+// }
